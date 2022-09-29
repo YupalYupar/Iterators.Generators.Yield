42a8b1b403815eb3d2dc1d1994ee6f6531529d7a
Khachatryan Marsel <marselvkhachatryan@gmail.com>
2022-09-30 02:41:28 +0300

    добавил еще 2 генератора

--- main.py
+++ main.py
@@ -22,13 +22,35 @@ class FlatIterator:
         return self.tuk[self.cursor][self.tuk_cursor - 1]
 
 
-def gen_list(a: list):
+# комперхеншн
+kok = [item for item in FlatIterator(nested_list)]
+print(f'Комперхеншн ==> {kok}')
+
+
+def gen_list(a: list):  # Генератор 1
     yield [i for pok in a for i in pok]
+
+
+def flat_generator(some_list: list):  # генератор 2
+    for i in some_list:
+        yield i
+
+
+def flat_generator2(*args, **kwargs):  # генератор 3
+    for hen in args, kwargs:
+        for egg in hen:
+            yield egg
 
 
 if __name__ == "__main__":
     gun = [ball for ball in FlatIterator(nested_list)]
-    print(gun)
+    print(f'итератор 1 ==> {gun}')
 
     for item in gen_list(nested_list):
-        print(item)
+        print(f'генератор 1 ==> {item}')
+
+    for item in flat_generator(nested_list):
+        print(f'генератор 2 ==> {item}')
+
+    for item in flat_generator2(nested_list):
+        print(f'генератор 3 ==> {item}')
